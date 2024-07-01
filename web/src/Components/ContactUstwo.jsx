b69f4d06dfed45a96101bf80cc413bdbac14cb1b
sumitkumar-ui <sumeetkuma8504@gmail.com>
2024-07-01 11:51:30 +0530

    changes

--- web/src/Components/ContactUstwo.jsx
+++ web/src/Components/ContactUstwo.jsx
@@ -20,7 +20,7 @@ const ContactUs = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://novanector.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
